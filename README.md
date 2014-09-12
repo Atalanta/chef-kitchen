5579ebca48d1f98d6d7f2a5176c91b9c36703db3
Stephen Nelson-Smith <stephen@atalanta-systems.com>
2014-09-12 00:54:18 -0400

    Initial creation of kitchen cookbook

---
+++ README.md
@@ -0,0 +1,16 @@
+# kitchen
+
+This cookbook sets up a machine for cookbook testing purposes.
+
+## Usage
+
+Simply add the default kitchen recipe to your node's run list, and run Chef.
+
+## To do
+
+- ChefDK
+- Ruby?
+- Vagrant
+- Virtualbox
+- Docker
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'kitchen'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures kitchen'
+long_description 'Installs/Configures kitchen'
+version          '0.1.0'
+
